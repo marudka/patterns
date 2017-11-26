2f2b140d014d288486412884ebb82c2432320ea2
Anna Welc <biuro@bluewolf.pl>
2017-11-26 12:27:19 +0100

    Module pattern - update

--- module/module.js
+++ module/module.js
@@ -19,6 +19,32 @@ var Module = (function() {
    }
 })();
 
+var Module2 = (function() {
+    var object = {};
+    var _counter = 0;
+
+    var _increment = function() {
+        _counter++;
+    };
+
+    var _getCounter = function() {
+        return _counter;
+    };
+
+    object.increment = function() {
+        _increment();
+    };
+    object.get = function() {
+        return _getCounter();
+    };
+
+    return object;
+})();
+
 console.log(Module.get()); // 0
 Module.increment();
 console.log(Module.get()); // 1
+
+console.log(Module2.get()); // 0
+Module2.increment();
+console.log(Module2.get()); // 1
